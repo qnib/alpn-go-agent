032482badf939edaa83f7f6c3ad5d48249a682f8
Christian Kniep <christian@qnib.org>
2016-02-13 16:51:49 +0100

    spin-off go-dckrimg

--- opt/qnib/gocd/agent/bin/start.sh
+++ opt/qnib/gocd/agent/bin/start.sh
@@ -1,13 +1,14 @@
 #!/usr/local/bin/dumb-init /bin/bash
 
 if [ "X${GOCD_AGENT_AUTOENABLE_KEY}" != "X" ];then
-    sed -i -e "s/agent.auto.register.key=.*/agent.auto.register.key=${GOCD_AGENT_AUTOENABLE_KEY}/" /opt/go-agent/config/autoregister.properties
+    GOCD_KEY=$(echo ${GOCD_AGENT_AUTOENABLE_KEY}| tr -d '"')
+    sed -i -e "s/agent.auto.register.key=.*/agent.auto.register.key=${GOCD_KEY}/" /opt/go-agent/config/autoregister.properties
 fi
 
 if [ "X${GOCD_LOCAL_DOCKERENGINE}" == "Xtrue" ];then
-    sed -i -e 's/agent.auto.register.resource=*/agent.auto.register.resources=docker-engine/' /opt/go-agent/config/autoregister.properties
+    sed -i -e 's/agent.auto.register.resources=.*/agent.auto.register.resources=docker-engine/' /opt/go-agent/config/autoregister.properties
 fi
 
-/opt/go-agent/agent.sh 2>&1 1>/var/log/gocd-agent.log
+/opt/go-agent/agent.sh 2>&1 1>/var/log/gocd-agent.log &
 echo $$ > /opt/go-agent/go-agent.pid
 tail -f /var/log/gocd-agent.log
