284cc17e67088f8e74f6ab752404a8a16b91128d
Christian Kniep <christian@qnib.org>
2016-05-24 11:00:16 +0200

    assemble BUILD_IMG_NAME from helper function

--- opt/qnib/gocd/helpers/gocd-functions.sh
+++ opt/qnib/gocd/helpers/gocd-functions.sh
@@ -3,10 +3,14 @@
 function assemble_build_img_name {
     # Create BUILD_IMG_NAME, which includes the git-hash and the revision of the pipeline
     export IMG_NAME=$(echo ${GO_PIPELINE_NAME} |awk -F'[\_\.]' '{print $1}')
-    if [ -z ${GO_REVISION} ];then
+    if [ ! -z ${GO_REVISION} ];then
+        export BUILD_IMG_NAME="${DOCKER_REPO-qnib}/${IMG_NAME}:${DOCKER_TAG}-${GO_REVISION}-rev${GO_PIPELINE_COUNTER}"
+    elif [ ! -z ${GO_REVISION_DOCKER} ];then
+        export BUILD_IMG_NAME="${DOCKER_REPO-qnib}/${IMG_NAME}:${DOCKER_TAG}-${GO_REVISION_DOCKER}-rev${GO_PIPELINE_COUNTER}"
+    elif [ ! -z ${GO_REVISION_DOCKER_} ];then
         export BUILD_IMG_NAME="${DOCKER_REPO-qnib}/${IMG_NAME}:${DOCKER_TAG}-${GO_REVISION_DOCKER_}-rev${GO_PIPELINE_COUNTER}"
     else
-        export BUILD_IMG_NAME="${DOCKER_REPO-qnib}/${IMG_NAME}:${DOCKER_TAG}-${GO_REVISION}-rev${GO_PIPELINE_COUNTER}"
+        export BUILD_IMG_NAME="${DOCKER_REPO-qnib}/${IMG_NAME}:${DOCKER_TAG}-rev${GO_PIPELINE_COUNTER}"
     fi
     echo ">> BUILD_IMG_NAME:${BUILD_IMG_NAME}"
 }
--- opt/qnib/gocd/tasks/docker/publish.sh
+++ opt/qnib/gocd/tasks/docker/publish.sh
@@ -2,13 +2,9 @@
 echo ">> Publish"
 set -xe
 
-IMG_NAME=$(echo ${GO_PIPELINE_NAME} |awk -F'[\_\.]' '{print $1}')
-if [ -z ${GO_REVISION} ];then
-    export BUILD_IMG_NAME="${DOCKER_REPO-gaikai}/${IMG_NAME}:${DOCKER_TAG}-${GO_REVISION_DOCKER_}-rev${GO_PIPELINE_COUNTER}"
-else
-    export BUILD_IMG_NAME="${DOCKER_REPO-gaikai}/${IMG_NAME}:${DOCKER_TAG}-${GO_REVISION}-rev${GO_PIPELINE_COUNTER}"
-fi
-echo ">> BUILD_IMG_NAME:${BUILD_IMG_NAME}"
+source /opt/qnib/gocd/helpers/gocd-functions.sh
+# Create BUILD_IMG_NAME, which includes the git-hash and the revision of the pipeline
+assemble_build_img_name
 
 
 if [ "X${SKIP_TAG_LATEST}" != "Xtrue" ];then
